a3d73cc040e57e219ecef98a15a90064801411c7
Afzal Basheer <appuafzal777@gmail.com>
2024-10-24 23:37:20 +0530

    completing js code

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ const showWinner = (userWin, userChoice, compChoice) => {
     userScorePara.innerText = userScore;
     msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
     msg.style.backgroundColor = "green";
+
   } else {
     compScore++;
     compScorePara.innerText = compScore;
@@ -31,3 +32,33 @@ const showWinner = (userWin, userChoice, compChoice) => {
     msg.style.backgroundColor = "red";
   }
 };
+
+const playGame = (userChoice) => {
+    //Generate computer choice
+    const compChoice = genCompChoice();
+  
+    if (userChoice === compChoice) {
+      //Draw Game
+      drawGame();
+    } else {
+      let userWin = true;
+      if (userChoice === "rock") {
+        //scissors, paper
+        userWin = compChoice === "paper" ? false : true;
+      } else if (userChoice === "paper") {
+        //rock, scissors
+        userWin = compChoice === "scissors" ? false : true;
+      } else {
+        //rock, paper
+        userWin = compChoice === "rock" ? false : true;
+      }
+      showWinner(userWin, userChoice, compChoice);
+    }
+  };
+  
+  choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+      const userChoice = choice.getAttribute("id");
+      playGame(userChoice);
+    });
+  });
